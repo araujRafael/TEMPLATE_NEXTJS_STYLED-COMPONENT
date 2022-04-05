43aa1313b077357c9c474ca03b2f6c78cfdff46e
Rafael Diniz <76079407+araujRafael@users.noreply.github.com>
2022-04-05 17:44:13 -0300

    Import GlobalStyled and fix component _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,12 @@ import {GlobalStyles} from '../styles/GlobalStyles'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles/>
+    </>
+  )
 }
 
 export default MyApp
