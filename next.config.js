9fafae913dd133a8131fa993d5a0bad13ba4af2f
Rafael Diniz <76079407+araujRafael@users.noreply.github.com>
2022-04-05 17:46:50 -0300

    Add Google fonts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  googleFonts: {
+    fonts: [
+      "https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,400&display=swap",
+    ],
+  }
 }
